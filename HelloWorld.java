afe24d033f0857d9999ec462af18dfd8ee9f94af
Srastanka Kumar Behera <srbehera@equinix.com>
2016-04-07 14:04:55 -0700

    Jenkins test

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package com.dotcms.plugin.rest;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
